79abd1aae75d3f2c6b445820410943e6f74a52de
Joris <jcwilson2021@gmail.com>
2025-10-24 22:28:11 -0400

    restored original test file content

--- HW/HW07b-LegacySystems/Test_Triangle.py
+++ HW/HW07b-LegacySystems/Test_Triangle.py
@@ -1,16 +1,32 @@
-# test_triangle.py
-import pytest
+# -*- coding: utf-8 -*-
+"""
+Updated Jan 21, 2018
+The primary goal of this file is to demonstrate a simple unittest implementation
+
+@author: jrr
+@author: rk
+"""
+
+import unittest
+
 from Triangle import classifyTriangle
 
-@pytest.mark.parametrize(
-    "a,b,c,expected",
-    [
-        (3, 4, 5, "Right"),
-        (5, 3, 4, "Right"),
-    ],
-)
-def test_right_triangles(a, b, c, expected):
-    assert classifyTriangle(a, b, c) == expected
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
-def test_equilateral_triangle():
-    assert classifyTriangle(1, 1, 1) == "Equilateral"
+class TestTriangles(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+
+    def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        
+    def testEquilateralTriangles(self): 
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
+

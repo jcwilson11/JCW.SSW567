3e9831c7ade168ee51268c9307f59b28503138fa
Joris <jcwilson2021@gmail.com>
2025-10-25 02:41:08 -0400

    updated tests

--- HW/HW07b-LegacySystems/Test_Triangle.py
+++ HW/HW07b-LegacySystems/Test_Triangle.py
@@ -8,25 +8,50 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 """
 
 import unittest
-
 from Triangle import classifyTriangle
 
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
-
 class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
+    # Valid triangle classifications
+    def test_equilateral(self):
+        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral')
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+    def test_isosceles(self):
+        self.assertEqual(classifyTriangle(2, 2, 3), 'Isosceles')
+        self.assertEqual(classifyTriangle(2, 3, 2), 'Isosceles')
+        self.assertEqual(classifyTriangle(3, 2, 2), 'Isosceles')
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    def test_scalene(self):
+        self.assertEqual(classifyTriangle(4, 5, 6), 'Scalene')
+
+    def test_right(self):
+        self.assertEqual(classifyTriangle(3, 4, 5), 'Right')
+        self.assertEqual(classifyTriangle(5, 3, 4), 'Right')
+        self.assertEqual(classifyTriangle(7, 24, 25), 'Right')
+
+    # Triangle inequality / degenerate
+    def test_not_a_triangle(self):
+        self.assertEqual(classifyTriangle(1, 2, 3), 'NotATriangle')  # degenerate
+        self.assertEqual(classifyTriangle(10, 15, 30), 'NotATriangle')
+
+    # Invalid inputs: range, sign, type
+    def test_invalid_range(self):
+        self.assertEqual(classifyTriangle(201, 10, 10), 'InvalidInput')
+        self.assertEqual(classifyTriangle(199, 200, 201), 'InvalidInput')
+
+    def test_invalid_sign_zero(self):
+        self.assertEqual(classifyTriangle(0, 1, 1), 'InvalidInput')
+        self.assertEqual(classifyTriangle(1, 0, 1), 'InvalidInput')
+        self.assertEqual(classifyTriangle(1, 1, 0), 'InvalidInput')
+        self.assertEqual(classifyTriangle(-1, 2, 2), 'InvalidInput')
+
+    def test_invalid_type(self):
+        self.assertEqual(classifyTriangle(3.0, 4, 5), 'InvalidInput')
+        with self.assertRaises((TypeError, AssertionError)):
+            self.assertEqual(classifyTriangle('3', 4, 5), 'InvalidInput')
+
+    # Upper boundary (allowed)
+    def test_upper_boundary(self):
+        self.assertEqual(classifyTriangle(200, 200, 200), 'Equilateral')
 
 if __name__ == '__main__':
-    print('Running unit tests')
     unittest.main()
-

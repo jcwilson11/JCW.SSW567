7dd087e8ecedc061929f81dc6d06e033a3f5f235
Joris <jcwilson2021@gmail.com>
2025-10-25 21:34:24 -0400

    updated tests

--- HW/HW07b-LegacySystems/Test_Triangle.py
+++ HW/HW07b-LegacySystems/Test_Triangle.py
@@ -11,47 +11,56 @@ import unittest
 from Triangle import classifyTriangle
 
 class TestTriangles(unittest.TestCase):
-    # Valid triangle classifications
-    def test_equilateral(self):
-        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral')
+    def test_equilateral_basic(self):
+        self.assertEqual(classifyTriangle(2, 2, 2), 'Equilateral')
 
-    def test_isosceles(self):
-        self.assertEqual(classifyTriangle(2, 2, 3), 'Isosceles')
-        self.assertEqual(classifyTriangle(2, 3, 2), 'Isosceles')
-        self.assertEqual(classifyTriangle(3, 2, 2), 'Isosceles')
+    def test_equilateral_upper_bound(self):
+        self.assertEqual(classifyTriangle(200, 200, 200), 'Equilateral')
 
-    def test_scalene(self):
+    def test_isosceles_ab(self):
+        self.assertEqual(classifyTriangle(2, 2, 3), 'Isoceles')
+
+    def test_isosceles_bc(self):
+        self.assertEqual(classifyTriangle(3, 5, 5), 'Isoceles')
+
+    def test_isosceles_ac(self):
+        self.assertEqual(classifyTriangle(3, 3, 5), 'Isoceles')
+
+    def test_scalene_basic(self):
         self.assertEqual(classifyTriangle(4, 5, 6), 'Scalene')
 
-    def test_right(self):
+    def test_scalene_another(self):
+        self.assertEqual(classifyTriangle(5, 7, 10), 'Scalene')
+
+    def test_right_345(self):
         self.assertEqual(classifyTriangle(3, 4, 5), 'Right')
+
+    def test_right_345_perm1(self):
         self.assertEqual(classifyTriangle(5, 3, 4), 'Right')
-        self.assertEqual(classifyTriangle(7, 24, 25), 'Right')
 
-    # Triangle inequality / degenerate
-    def test_not_a_triangle(self):
-        self.assertEqual(classifyTriangle(1, 2, 3), 'NotATriangle')  # degenerate
-        self.assertEqual(classifyTriangle(10, 15, 30), 'NotATriangle')
+    def test_right_6810(self):
+        self.assertEqual(classifyTriangle(6, 8, 10), 'Right')
 
-    # Invalid inputs: range, sign, type
-    def test_invalid_range(self):
+    def test_not_triangle_equal_sum(self):
+        self.assertEqual(classifyTriangle(1, 2, 3), 'NotATriangle')
+
+    def test_not_triangle_big_side(self):
+        self.assertEqual(classifyTriangle(10, 1, 1), 'NotATriangle')
+
+    def test_not_triangle_upper_bound_mixed(self):
+        self.assertEqual(classifyTriangle(200, 1, 1), 'NotATriangle')
+
+    def test_invalid_over_200(self):
         self.assertEqual(classifyTriangle(201, 10, 10), 'InvalidInput')
-        self.assertEqual(classifyTriangle(199, 200, 201), 'InvalidInput')
 
-    def test_invalid_sign_zero(self):
-        self.assertEqual(classifyTriangle(0, 1, 1), 'InvalidInput')
-        self.assertEqual(classifyTriangle(1, 0, 1), 'InvalidInput')
-        self.assertEqual(classifyTriangle(1, 1, 0), 'InvalidInput')
-        self.assertEqual(classifyTriangle(-1, 2, 2), 'InvalidInput')
+    def test_invalid_zero(self):
+        self.assertEqual(classifyTriangle(0, 2, 3), 'InvalidInput')
 
-    def test_invalid_type(self):
-        self.assertEqual(classifyTriangle(3.0, 4, 5), 'InvalidInput')
-        with self.assertRaises((TypeError, AssertionError)):
-            self.assertEqual(classifyTriangle('3', 4, 5), 'InvalidInput')
+    def test_invalid_negative(self):
+        self.assertEqual(classifyTriangle(-1, 2, 3), 'InvalidInput')
 
-    # Upper boundary (allowed)
-    def test_upper_boundary(self):
-        self.assertEqual(classifyTriangle(200, 200, 200), 'Equilateral')
+    def test_invalid_non_integer(self):
+        self.assertEqual(classifyTriangle(3.5, 4, 5), 'InvalidInput')
 
 if __name__ == '__main__':
     unittest.main()
